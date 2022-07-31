2ca88f80078ee56224a8f6b89e12162d3f667840
zenion <joshstout@gmail.com>
2022-07-30 22:08:19 -0700

    try

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
 const fs = require("fs");
 
 console.log("WORKSPACE LOCATION: " + process.env.GITHUB_WORKSPACE);
-
 console.log(fs.readFileSync(process.env.GITHUB_WORKSPACE));
